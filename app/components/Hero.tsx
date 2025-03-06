382b47c799df83234e9827f64aaa5889ec436305
Promise Ibeh <ibehpromise3d@gmail.com>
2025-03-06 03:19:43 +0100

    added youtube video

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,18 +1,49 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 export default function Hero() {
+  const [showVideo, setShowVideo] = useState(false);
+
   return (
-    <div className="hero bg-base-200 min-h-screen bg-image">
+    <div className="hero bg-base-200 min-h-screen bg-image flex flex-col items-center justify-center">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold">The Developer Api Emulator</h1>
+          <h1 className="text-5xl font-bold">The Developer API Emulator</h1>
           <p className="py-6">
             Free fake and reliable API for testing and prototyping. Powered by
-            JSON nextjs engine
+            JSON Next.js engine.
           </p>
-          <button className="btn btn-primary">How to use</button>
+          <button
+            className="btn btn-primary"
+            onClick={() => setShowVideo(true)}
+          >
+            How to use
+          </button>
         </div>
       </div>
+
+      {/* Video Popup */}
+      {showVideo && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75">
+          <div className="relative bg-white p-4 rounded-lg">
+            <button
+              className="absolute top-2 right-2 text-black text-2xl"
+              onClick={() => setShowVideo(false)}
+            >
+              ✖
+            </button>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/GUgvTWNw0f4?si=Xs2V0tq66yljYkYl"
+              title="YouTube Video"
+              frameBorder="0"
+              allowFullScreen
+              className="rounded-lg"
+            ></iframe>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
